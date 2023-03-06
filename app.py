d812b255bf52a3150cc22fb7dd42a9b3bf74b1b6
shubanms <msshuban4@gmail.com>
2023-03-06 16:35:57 +0530

    changes

--- app.py
+++ app.py
@@ -1,46 +1,17 @@
 #app.py
-from flask import Flask, flash, request, redirect, url_for, render_template
-import urllib.request
-import os
-from werkzeug.utils import secure_filename
- 
+from flask import Flask,render_template
+from forms import RegistrationForm,LoginForm
+
 app = Flask(__name__)
- 
-UPLOAD_FOLDER = 'static/uploads/'
- 
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
- 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
- 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-     
- 
+
 @app.route('/')
 def home():
     return render_template('personal.html')
  
-@app.route('/', methods=['POST'])
-def upload_image():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-    else:
-        flash('Allowed image types are - png, jpg, jpeg, gif')
-        return redirect(request.url)
- 
-@app.route('/display/<filename>')
-def display_image(filename):
-    #print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
- 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html' , title="Register",form = form)
+
 if __name__ == "__main__":
     app.run()
